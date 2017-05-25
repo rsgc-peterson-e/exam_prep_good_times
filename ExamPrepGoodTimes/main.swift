d4f3eff7df3cbf5b2cda710826e6febca009c349
Russell Gordon <rgordon@rsgc.on.ca>
2017-05-25 13:34:51 -0400

    Initial Commit

---
+++ ExamPrepGoodTimes/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ExamPrepGoodTimes
+//
+//  Created by Russell Gordon on 5/25/17.
+//  Copyright © 2017 Russell Gordon. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
